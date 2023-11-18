07677b8a87a5fbb7e4f83f44c33d6270556601cf
Xiangyu Qin <qinx@umich.edu>
2023-11-18 15:51:18 -0500

    demo implemented

--- clients/panel_test.py
+++ clients/panel_test.py
@@ -8,6 +8,7 @@ import time,json
 import networkx as nx
 import traceback
 from collections import defaultdict
+import pprint
 
 # we want to use bootstrap/template, tell Panel to load up what we need
 pn.extension(design='bootstrap')
@@ -49,7 +50,8 @@ predValueKey = "value"
 
 def update():
     try:
-        global game, static_text, network_view, tree_view, info_view, hints_view, predDict
+        global game, static_text, network_view, tree_view, info_view, hints_view, predDict, robo_time_chart, robo_expiry_sorted
+        robo_expiry_sorted.object = getRoboSorted()
         gt = game.getGameTime()
         network_view.object = game.getNetwork()
         tree_view.object = game.getTree()
@@ -57,22 +59,24 @@ def update():
         hints_view.object = game.getHints()
         allPred = game.getAllPredictionHints()
         
-        predDict = defaultdict(dict)
+        predDict = defaultdict(lambda: defaultdict(list))
         for pred in allPred:
             roboId = pred[predIdKey]
             predTime = pred[predTimeKey]
             value = pred[predValueKey]
-            predDict[roboId][predTime] = value
+            predDict[roboId][predTimeKey].append(predTime)
+            predDict[roboId][predValueKey].append(value)
         
         hints_view.object = predDict
+        robo_time_chart.object = getTimeChart()
 
-        static_text.value = "Time left: " + str(gt['unitsleft'])
+        static_text.value = "Current time: " + str(100 - gt['unitsleft'])
     except:
         print(traceback.format_exc())
 
 def go_clicked(event):
     try:
-        global game, network, tree, info, hints
+        global game, network, tree, info, hints, robo_expiry_sorted
         go_button.disabled = True
         uname = username_input.value
         if (uname == ""):
@@ -104,7 +108,8 @@ def go_clicked(event):
             time.sleep(1) # sleep 1 second at a time, wait for the game to start
 
         # run a check every 5 seconds
-        cb = pn.state.add_periodic_callback(update, 5000, timeout=60000)
+        robo_expiry_sorted.object = getRoboSorted()
+        cb = pn.state.add_periodic_callback(update, 5000, timeout=600000)
     except:
         #print(traceback.format_exc())
         return
@@ -120,20 +125,38 @@ network_view = pn.pane.JSON({"message":"waiting for game to start"})
 tree_view = pn.pane.JSON({"message":"waiting for game to start"})
 info_view = pn.pane.DataFrame()
 hints_view = pn.pane.JSON({"message":"waiting for game to start"})
-robo_time_chart = pn.pane.JSON({"message":"waiting for game to start"})
+robo_time_chart = pn.pane.Vega(None)
+robo_expiry_sorted = pn.pane.Vega(None)
+curr_selected_robot = 1
 
 maincol = pn.Column()
 
-def getTimeChart(roboNum=2):
+def getTimeChart():
+    global curr_selected_robot, predDict
+    # print(f"\n\n\n textInput = {textInput} \n\n\n")
+    
+    # try:
+    #     robotId = int(textInput)
+    # except:
+    #     return 'Please enter an integer'
+    
+    robotId = curr_selected_robot
+        
     global predDict
     if predDict is None:
         return None
-    if roboNum not in predDict:
+    if robotId not in predDict:
         return "No data found for this robot yet"
-    robo_df = pd.DataFrame.from_dict(predDict[roboNum])
+
+    print(predDict[robotId])
+    
+    robo_df = pd.DataFrame.from_dict(predDict[robotId])
     points = alt.Chart(robo_df).mark_circle(color="red").encode(
-    x='time:Q',
-    y='value:Q',
+        x=alt.X('time:Q', scale=alt.Scale(domain=[0, 100])),
+        y=alt.Y('value:Q', scale=alt.Scale(domain=[0, 100])),
+        tooltip=['time', 'value']
+    ).properties(
+        title=f"Robot {robotId}'s data"
     )
 
     line = alt.Chart(robo_df).mark_line(interpolate="monotone").encode(
@@ -141,15 +164,71 @@ def getTimeChart(roboNum=2):
         y='value:Q'
     )
 
-    expiryLine = alt.Chart(pd.DataFrame({"x": [70]})).mark_rule().encode(
+    robotInfo = game.getRobotInfo()
+    currRobot = robotInfo[robotInfo['id'] == robotId]
+    expiry = currRobot["expires"].iloc[0]
+    expiryLine = alt.Chart(pd.DataFrame({"x": [expiry]})).mark_rule().encode(
         x="x:Q"
     )
 
     return line + points + expiryLine
 
-refreshBtn = pn.widgets.Button(name="refresh time chart")
+def updateCurrSelected(textInput):
+    global curr_selected_robot, robo_time_chart
+    try:
+        curr_selected_robot = int(textInput)
+        robo_time_chart.object = getTimeChart()
+        return "Updated!"
+    except:
+        return "Please enter an integer"
 
-row = pn.Row(pn.bind(getTimeChart, refreshBtn))
+def getFilteredRobotRecords(sortedRobotRecords, robotDegrees, currTime = 20, numCount = 20):
+    filteredRobotRecords = []
+    currRank = 1
+    currCount = 0
+    for robot in sortedRobotRecords:
+        if robot["id"] >= 100:
+            break
+        if robot["expires"] < currTime:
+            continue
+        robot["rank"] = currRank
+        robot["degree"] = robotDegrees[robot["id"]]
+        currRank += 1
+        filteredRobotRecords.append(robot)
+        currCount += 1
+        if currCount >= numCount:
+            break
+        
+    return filteredRobotRecords
+
+def getRoboSorted():
+    global game
+    if game is None:
+        return None
+    robots = game.getRobotInfo()
+    network = game.getNetwork()
+    socialnet = nx.node_link_graph(network)
+    robotDegrees = {node:val for (node, val) in socialnet.degree()}
+    robotRecords = robots.to_dict("records")
+    sortedRobotRecords = sorted(robotRecords, key=lambda x:x["expires"])
+    
+    gt = game.getGameTime()
+    currTime = 100 - gt['unitsleft']
+    
+    filteredRobotRecords = getFilteredRobotRecords(sortedRobotRecords, robotDegrees, currTime=currTime)
+    
+    robotDfWithRank = pd.DataFrame.from_records(filteredRobotRecords)
+    return alt.Chart(robotDfWithRank).mark_circle(size=100).encode(
+        alt.X('rank', axis=None),
+        size="degree",
+        tooltip=['name', 'expires', 'rank', "id", "degree"]
+    ).properties(
+        width=800
+    )
+
+robotIdInput = pn.widgets.TextInput(placeholder="Robot ID")
+
+rowChart = pn.Row(pn.bind(updateCurrSelected, robotIdInput))
 
 
 grid = pn.GridBox(ncols=2,nrows=3)
@@ -159,8 +238,11 @@ grid.append(info_view)
 grid.append(hints_view)
 
 # timechart = getTimeChart(3)
-maincol.append(refreshBtn)
-maincol.append(row)
+maincol.append(robo_time_chart)
+maincol.append(rowChart)
+maincol.append(robotIdInput)
+maincol.append("Upcoming robot and its popularity in the social network:")
+maincol.append(robo_expiry_sorted)
 maincol.append(grid)
 
 template.main.append(maincol)
